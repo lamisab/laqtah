eb50cf5badcdf306830312dd23388432556ca94e
Lamis Abdullah <102637669+lamisab@users.noreply.github.com>
2022-08-14 04:43:01 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,70 +1,74 @@
-import fastifyAutoload from '@fastify/autoload';
-import fastifySensible from '@fastify/sensible';
-import fastifySwagger from '@fastify/swagger';
-import fastifyJwt from '@fastify/jwt';
-import { ajvTypeBoxPlugin, TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
-import fastify from 'fastify';
-import { join } from 'path';
+import fastifyAutoload from "@fastify/autoload";
+import fastifySensible from "@fastify/sensible";
+import fastifySwagger from "@fastify/swagger";
+import fastifyJwt from "@fastify/jwt";
+import {
+  ajvTypeBoxPlugin,
+  TypeBoxTypeProvider,
+} from "@fastify/type-provider-typebox";
+import fastify from "fastify";
+import { join } from "path";
 
 export const server = fastify({
-	logger: true,
-	ajv: {
-		customOptions: {
-			removeAdditional: 'all',
-			ownProperties: true,
-		},
-		plugins: [ajvTypeBoxPlugin],
-	},
+  logger: true,
+  ajv: {
+    customOptions: {
+      removeAdditional: "all",
+      ownProperties: true,
+    },
+    plugins: [ajvTypeBoxPlugin],
+  },
 }).withTypeProvider<TypeBoxTypeProvider>();
 
-server.register(require("@fastify-cors"),{
-	origin: "*",
-	method: ["GET"],
+server.register(require("@fastify/cors"), {
+  origin: "*",
+  methods: ["GET"],
 });
 
 server.register(fastifyJwt, {
-	secret: '123',
+  secret: "123",
 });
 server.register(fastifySwagger, {
-	routePrefix: '/docs',
-	exposeRoute: true,
-	mode: 'dynamic',
-	openapi: {
-		info: {
-			title: 'Laqtah API',
-			version: '0.0.1',
-		},
-		security: [
-			{
-				bearerAuth: [],
-			},
-		],
-		components: {
-			securitySchemes: {
-				bearerAuth: {
-					type: 'http',
-					scheme: 'bearer',
-					bearerFormat: 'JWT',
-				},
-			},
-		},
-	},
+  routePrefix: "/docs",
+  exposeRoute: true,
+  mode: "dynamic",
+  openapi: {
+    info: {
+      title: "Laqtah API",
+      version: "0.0.1",
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
 });
 
 server.register(fastifySensible);
 server.register(fastifyAutoload, {
-	dir: join(__dirname, 'routes'),
+  dir: join(__dirname, "routes"),
 });
 const port: any = process.env.PORT ?? process.env.$PORT ?? 3009;
 
 export function listen() {
-	server
-		.listen({
-			port: port,
-			host: '0.0.0.0',
-		})
-		.catch((err) => {
-			server.log.error(err);
-			process.exit(1);
-		});
+  server
+    .listen({
+      port: port,
+      host: "0.0.0.0",
+    })
+    .catch((err) => {
+      server.log.error(err);
+      process.exit(1);
+    });
 }
+
